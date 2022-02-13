cc688ecd2683e2efccda2dd72bd58b01cb19dd49
BlueveryPi <78254389+BlueveryPi@users.noreply.github.com>
2022-02-13 23:43:45 +0900

    minor bugfix
    bugfix for indexerror.

---
+++ avocado.py
@@ -0,0 +1,82 @@
+import os
+import argparse
+
+parser=argparse.ArgumentParser(description="Process Avocado.")
+
+parser.add_argument("file", type=str, help="Code file to process.")
+
+args=parser.parse_args()
+path=args.file
+
+if not os.path.exists(path):
+    raise TypeError("Inappropriate argument type")
+elif not os.path.isfile(path):
+    raise TypeError("Argument is not a file")
+else:
+    if not path.endswith(".avo"):
+        raise TypeError("Inappropriate file type")
+    else:
+        with open(path, "r+", encoding="utf-8") as f:
+
+            code=f.read().replace("\n", "")
+            code=[char for char in code]
+            memory=[]
+
+            for i in range(32768):
+                memory.append(0)
+            pointer=0
+
+            for i, command in enumerate(code):
+                if command=="쑥":
+                    if code[i+1]!="쑥":
+                        raise SyntaxError(f"Unmatched '쑥' in {i}")
+                    else:
+                        code.pop(i+1)
+
+            i=-1
+            
+            while i<len(code)-1:
+                i+=1
+                command=code[i]
+                #print(command)
+
+                if command=="아":
+                    if pointer<32768:
+                        pointer+=1
+                    else:
+                        raise OverflowError("Pointer too large")
+
+                elif command=="보":
+                    if pointer>0:
+                        pointer-=1
+                    else:
+                        raise OverflowError("Pointer too small")
+
+                elif command=="카":
+                    memory[pointer]+=1
+
+                elif command=="도":
+                    memory[pointer]-=1
+
+                elif command=="쑥":
+                    if type(memory[pointer])==int:
+                        memory[pointer]=float(memory[pointer])
+                    else:
+                        memory[pointer]=int(memory[pointer])
+
+                elif command=="!":
+                    if type(memory[pointer])==float:
+                        print(chr(int(memory[pointer])), end="")
+                    else:
+                        print(int(memory[pointer]), end="")
+
+                elif command=="?":
+                    if int(memory[pointer])>len(code)-1:
+                        raise IndexError(f"Jump to line {int(memory[pointer])} failed: Index too large")
+                    elif int(memory[pointer])<0:
+                        raise IndexError(f"Jump to line {int(memory[pointer])} failed: Index too small")
+                    else:
+                        i=int(memory[pointer])
+
+                else:
+                    raise SyntaxError(f"Invalid syntax in {i}")
